77ba6c2bf579dc07991fa34dcda869e1b7a6ee06
sara <ups@gmail.com>
2019-10-28 13:07:46 +0100

    outline

---
+++ assing4.py
@@ -0,0 +1,16 @@
+#FUNCTIONS
+#1.abundancefunction
+
+#2.split into 2files
+
+#3. hard trime (reuse trim from ass 2)
+
+#4. use BWA http://bio-bwa.sourceforge.net/
+
+
+#5merge samfiles
+
+
+#6.sorts sam by cr
+
+#7. comp num of reads aligh
