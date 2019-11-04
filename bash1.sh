8df0a978fad79270a64e804f3213a1d65abb76cb
Kuba <31893096+jdwidawski@users.noreply.github.com>
2019-11-04 10:30:43 +0100

    Modify bash files

--- bash1.sh
+++ bash1.sh
@@ -2,4 +2,4 @@
 # directory assign4_output contains all the split .fasta sequences
 # sacch.fasta - complete saccharomyces genome
 bwa index -p sacch -a bwtsw sacch.fasta;
-for file in assign4_output/*.fasta; do bwa aln -t 4 sacch $file > $file.bwa| bwa samse sacch $file.bwa $file > $file.sam ;done
+for f in assign4_output/*.fasta; do bwa aln -t 4 sacch $f > $f.bwa| bwa samse sacch $f.bwa $f > $f.sam ;done;
