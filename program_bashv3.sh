782eca30cf29f2fafcf7eac376ca4633c76b408d
sduarrir <sduarrir@gmail.com>
2019-11-12 00:16:41 +0100

    bash script upd

--- program_bashv3.sh
+++ program_bashv3.sh
@@ -2,8 +2,10 @@
 ##ASSIGNMENT 4
 
 
-
-
+if [ $# -ne 2 ]; then
+ echo "The program needs to arguments, genome file and reads file (in this order)";
+ exit;
+fi
 
 
 ## 1. Counts the abundance of each possible 3-mers (histogram)
@@ -12,8 +14,10 @@ if [ ! -d assign4_temp ]; then
   mkdir assign4_temp;
 fi
 
+#save arguments
+FILE=$2; #FILE WITH THE READS IS THE SECOND ARGUMENT
+GENOME=$1
 # Count 3-mers in file
-FILE=sequence.fasta;
 grep -v "^>" $FILE | tr '[:lower:]' '[:upper:]' | fold -w 3 > trimers.1.txt; #orf1
 grep -v "^>" $FILE | tr '[:lower:]' '[:upper:]' | fold -w 1 | tail -n +2 | tr -d '\n' | fold -w 3 > trimers.2.txt; #orf2
 grep -v "^>" $FILE | tr '[:lower:]' '[:upper:]' | fold -w 1 | tail -n +3 | tr -d '\n' | fold -w 3 > trimers.3.txt; #orf3
@@ -24,16 +28,15 @@ rm trimers.3.txt;
 
 ## remove those that are not trimers len < 3 (max number of lines of stats should be 64)
 
-at stats.txt | awk -F ' ' 'length($2)== 3{print$1,$2}' #now need to save it in a file!!
+cat stats.txt | awk -F ' ' 'length($2)== 3{print$1,$2}' > stats_filt.txt # now need to save it in a file!!
 
 ## 2. Splits the input in FASTA/FASTQ into files of only 1 sequence each
 printf "\nCreating seperate fasta files...\n";
 # Split to seperate fasta files 
 csplit sequence.fasta '/>/' {*} --prefix='./assign4_temp/seq' --suffix-format='%d.fasta' --elide-empty-files -s #silent
-#this could lead into problems if in the header theres a > symbol
+#this could lead into problems if in the header there is a > symbol
 
-#head -n 1600 sequence.fasta | awk '/>/{x="assign4_temp/fasta_seq"++i".fasta";}{print > x;}';
-#that just created a file dit not split as it suposed to
+
 
 ##3. Hard-trims (from the right) all sequences from all files 20nt
 # loop to make all Nucleotides uppercase #changed the way of cutting (we avoid trimmind after id newline)
@@ -48,32 +51,38 @@ done
 ##4. Using the genome mapping tool BWA and the reference genome of the Scaromice Cerevisiae (any strain will do), aligns each of the files producing its corresponding SAM file.
 printf "\nIndexing Saccharomyces (S288C) genome and creating alignment files...\n";
  # index sacch genome sacch as db index(p) indicate we will be woroking with a whole genome(-a bwtsw)
-bwa index -p sacch -a bwtsw sacch.fasta 2> /dev/null;
+bwa index -p sacch -a bwtsw $GENOME 2> /dev/null;
 
 # for each fasta create sam allignment file
-for file in assign4_temp/*.fasta; do bwa bwasw sacch $file > $file.sam 2> /dev/null ;rm $file;done 
-
+#for file in assign4_temp/*.fasta; do bwa bwasw sacch $file > $file.sam 2> /dev/null ;rm $file;done
+for file in assign4_temp/*.fasta; do bwa bwasw sacch $file > $file.sam 2> /dev/null ;done
 
 #5. Merge all SAM files ignoring headers (using Linux tools)
 printf "\nRemoving SAM headers and creating a merged SAM file (merged-file.sam)...\n";
 # creates merged sam file from all the separate sam files in assign4_temp
+
 for file in assign4_temp/*.sam; do
   str="$(cat $file)"; # read file
   str="$(grep -v '^@' <<<$str)"; # remove header lines
   IFS=$'\t'; # space is set as delimiter
   read -ra ADDR <<< "$str"; # str is read into an array as tokens separated by IFS
   cigar="${ADDR[5]}"; # Look for the cigar str in SAM
-  echo -n > merged-file.sam; # open new file "merged-file.sam"
+  echo -n > merged-file.sam; # open new file "merged-file.sam" !!!! THIS WILL DELETE PREVIOUS FILES!!!!
   if [ "$cigar" != "*" ]; then # If cigar is not empty
     echo "$str" >> merged-file.sam; # append the sam file to merged-sam file
   fi
-  rm $file; # remove the file
+  #rm $file; # remove the file
 done;
 
 
 #6. Sorts the SAM file by chromosome and position
-#7. Compute how many reads have been aligned (using Linux tools)
+samtools sort merged-file.sam > sorted-merged-file.sam
+#3rd colum indicates th cromosome (i think they're indexed) 4th starting position
 
+#7. Compute how many reads have been aligned (using Linux tools)
+wc -l sorted-merged-file.sam
+
+##THIS IS MISSING
 
 
 ##################### STILL NEED TO SORT THE MERGED SAM FILE #############################
@@ -97,7 +106,5 @@ done;
 printf "\nDone\n";
 printf "\nFirst 10 lines from the sorted-merged-file.sam:\n\n";
 head sorted-merged-file.sam;
-
-
 
 
