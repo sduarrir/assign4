03b9e0840e86337acbfc98f428a1ac13a6cf2909
sara <ups@gmail.com>
2019-11-12 13:59:32 +0100

    stuff in place

--- old code/bash2.sh
+++ old code/bash2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#merge sam
 # directory assign4_output contains all the split .fasta sequences
 # creates merged sam file from all the separate sam files in assign4_output
 for file in assign4_output/*.sam; do grep -v '^@' $file >> merged-file.sam ;done
